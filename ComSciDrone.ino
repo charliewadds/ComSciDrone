035346283804e09f7073ffe7c11634ae4ca852af
charlie <charliewadds@me.com>
2019-04-12 18:15:00 -0400

    add motors to sketch

--- ComSciDrone.ino
+++ ComSciDrone.ino
@@ -5,6 +5,7 @@
 
 #include "Wire.h"
 #include "INOsetup.h"
+#include "MotorControl.h"
 
 
 
@@ -32,7 +33,15 @@ Serial.print(p);
 Serial.print("      roll ");
 Serial.print(r);
 Serial.println();
-
+    Serial.print("      M1 ");
+Serial.print(motor[0]);
+    Serial.print("      M2 ");
+Serial.print(motor[1]);
+    Serial.print("      M3 ");
+Serial.print(motor[2]);
+    Serial.print("      M4 ");
+Serial.print(motor[3]);
+Serial.println();
 
 
 

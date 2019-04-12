e2d473c0f302e35b27b6a26eb1328f66f738d884
charlie <charliewadds@me.com>
2019-04-12 18:22:13 -0400

    a

--- ComSciDrone.ino
+++ ComSciDrone.ino
@@ -27,20 +27,20 @@ void setup() {
 void loop() {
     updateYPR(); //F() is supposed to save sram
 Serial.print(F("yaw "));
-Serial.print(F(y));
+Serial.print(y);
 Serial.print(F("      pitch "));
-Serial.print(F(p));
+Serial.print(p);
 Serial.print(F("      roll "));
 Serial.print(r);
 Serial.println();
     Serial.print(F("      M1 "));
-Serial.print(F(motors[0]));
+Serial.print(motors[0]);
     Serial.print(F("      M2 "));
-Serial.print(F(motors[1]));
+Serial.print(motors[1]);
     Serial.print(F("      M3 "));
-Serial.print(F(motors[2]));
+Serial.print(motors[2]);
     Serial.print(F("      M4 "));
-Serial.print(F(motors[3]));
+Serial.print(motors[3]);
 Serial.println();
     delay(100);//only for debugging
 

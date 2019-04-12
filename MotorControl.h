1951ff333ad9efa266b768f2e3e9c08e27c4365a
charlie <charliewadds@me.com>
2019-04-12 18:37:01 -0400

    i

--- MotorControl.h
+++ MotorControl.h
@@ -22,24 +22,24 @@ float r;
  *
  */
 
-float* stabilize(float t, float s){//definetly going to have problems with float* vs float** etc
+float* stabilize(float MY,float MP,float MR,float t, float s){//definetly going to have problems with float* vs float** etc
     
     
-    if(p<0-t){
-        motors[2]+p*s;
-        motors[3]+p*s;
+    if(MP<0-t){
+        motors[2]+MP*s;
+        motors[3]+MP*s;
     }
-    if(p>0+t){
-        motors[0]+p*s;
-        motors[1]+p*s;
+    if(MP>0+t){
+        motors[0]+MP*s;
+        motors[1]+MP*s;
     }
-    if(r<0-t){
-        motors[0]+r*s;
-        motors[2]+r*s;
+    if(MR<0-t){
+        motors[0]+MR*s;
+        motors[2]+MR*s;
     }
-    if(r>0+t){
-        motors[1]+r*s;
-        motors[3]+r*s;
+    if(MR>0+t){
+        motors[1]+MR*s;
+        motors[3]+MR*s;
     }
     return motors;
 }
